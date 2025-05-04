0336fee9dfb17cde70ff16fdb518bf4a2b799a5c
Иван Глазунов <q-q@inbox.ru>
2025-05-04 15:18:51 +0300

    Сделал print(), но с arrPersons = ArrayList(). Надо PeopleManager.arrPersons перевести в Set<> (или что получше) и настроить сортировку. А также доделать остальные пункты.

--- src/main/commands/Commands.java
+++ src/main/commands/Commands.java
@@ -1,37 +1,68 @@
 package main.commands;
 
-import java.util.Scanner;
+import main.persons.Person;
+
+import java.util.*;
 
 public class Commands {
-    Scanner scanner = new Scanner(System.in);
 
-    public void inCommand() {
-        String command = scanner.nextLine();
-        switch (command) {
-            case "in":
-                in();
-            case "del":
-                del();
-            case "count":
-                count();
-            case "avg":
-                avg();
-            case "median":
-                median();
-            case "young":
-                young();
-            case "old":
-                old();
-            case "print":
-                print();
-            case "help":
-                help();
-            case "exit":
-                exit();
-            default:
-                System.out.println("Неизвестная команда. Попробуйте снова");
+    public void inCommand(PeopleManager manager) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nВведите команду: ");
+        String command = null;
+        try {
+            command = scanner.nextLine();
+
+
+            switch (command) {
+                case "in" -> {
+                    in();
+                    inCommand(manager);
+                }
+                case "del" -> {
+                    del();
+                    inCommand(manager);
+                }
+                case "count" -> {
+                    count();
+                    inCommand(manager);
+                }
+                case "avg" -> {
+                    avg();
+                    inCommand(manager);
+                }
+                case "median" -> {
+                    median();
+                    inCommand(manager);
+                }
+                case "young" -> {
+                    young();
+                    inCommand(manager);
+                }
+                case "old" -> {
+                    old();
+                    inCommand(manager);
+                }
+                case "print" -> {
+                    print(manager);
+                    inCommand(manager);
+                }
+                case "help" -> {
+                    help();
+                    inCommand(manager);
+                }
+                case "exit" -> exit();
+                default -> {
+                    System.out.println("Неизвестная команда. Попробуйте снова");
+                    scanner.close();
+                    inCommand(manager);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Неизвестная команда. Попробуйте снова");
         }
     }
+
 
     public void in() {
     }
@@ -54,27 +85,40 @@ public class Commands {
     public void old() {
     }
 
-    public void print() {
+
+    public void print(PeopleManager manager) {
+        System.out.println("Список юзеров:");
+        ArrayList<Person> tmpArr = manager.getArrPersons();
+
+        if (tmpArr != null && !tmpArr.isEmpty()) {
+            for (Person p : tmpArr) {
+                System.out.println(p);
+//                System.out.println(p.getName()); // Вывод объекта Person
+//                System.out.println(p.getAge()); // Вывод объекта Person
+//                System.out.println(p.getPassport()); // Вывод объекта Person
+            }
+        } else {
+            System.out.println("Список пуст или равен null.");
+        }
     }
 
     public void help() {
-        System.out.println(
-                """
-                        \n**************************************           \s
-                                      ИНСТРУКЦИЯ
-                        Доступные команды:
-                        in      — ввод нового пользователя
-                        del     — удаление пользователя по номеру паспорта
-                        count   — показать количество пользователей
-                        avg     — вывести средний возраст пользователей
-                        median  — вывести медианный возраст пользователей
-                        young   — самый молодой пользователь
-                        old     — самый старый пользователь
-                        print   — список пользователей по возрастанию возраста
-                        help    — эта справка
-                        exit    — выход из программы
-                        **************************************
-                        """);
+        System.out.println("""
+                \n**************************************\s
+                              ИНСТРУКЦИЯ
+                Доступные команды:
+                1. in      — ввод нового пользователя
+                2. del     — удаление пользователя по номеру паспорта
+                3. count   — показать количество пользователей
+                4. avg     — вывести средний возраст пользователей
+                5. median  — вывести медианный возраст пользователей
+                6. young   — самый молодой пользователь
+                7. old     — самый старый пользователь
+                8. print   — список пользователей по возрастанию возраста
+                9. help    — эта справка
+                10. exit    — выход из программы
+                **************************************"""
+        );
     }
 
     public void exit() {
@@ -82,3 +126,4 @@ public class Commands {
         System.exit(0);
     }
 }
+

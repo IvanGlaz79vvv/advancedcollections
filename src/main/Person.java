e6c54ba0fcff2daff06be6f3d305a225e10ff651
Иван Глазунов <q-q@inbox.ru>
2025-05-04 03:40:54 +0300

    Start

---
+++ src/main/Person.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Person {
+    String name;
+    int age;
+    int passport;
+
+    public Person(String name, int age, int passport) {
+        this.name = name;
+        this.age = age;
+        this.passport = passport;
+    }
+}

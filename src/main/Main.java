cba3b654b85a8ace6f86d0749eed2398e2f4939b
Иван Глазунов <q-q@inbox.ru>
2025-05-08 21:38:05 +0300

    Готово v1.final

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,24 +12,6 @@ public class Main {
         Commands commands = new Commands();
         PeopleManager manager = new PeopleManager();
 
-        Person ivan = new Person(1234567890L, "Ivan", 45);
-        Person ivan2 = new Person(1234567891L, "Ivan", 45);
-        Person alex = new Person(1234567892L, "Alex", 20);
-        Person anastasia = new Person(1234567893L, "Anastasia", 30);
-        Person petr = new Person(1234567894L, "Petr", 15);
-        Person olga = new Person(1234567895L, "Olga", 12);
-        Person igor = new Person(1234567896L, "Igor", 70);
-
-        manager.getArrPersons().add(ivan);
-        manager.getArrPersons().add(ivan2);
-        manager.getArrPersons().add(alex);
-        manager.getArrPersons().add(anastasia);
-        manager.getArrPersons().add(petr);
-        manager.getArrPersons().add(olga);
-        manager.getArrPersons().add(igor);
-
-
-        commands.print(manager);
         commands.start(manager);
     }
 }
